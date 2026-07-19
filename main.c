0ae15172d6b9fa3fe6457ce88c4b0a9d7bc868db
YersonGames <prino98k@gmail.com>
2026-07-19 05:59:12 -0400

    aa

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+#include <math.h>
+
+inline int sum(int a,int b);
+
+void message1();
+void message2();
+void message3();
+
+void ln();
+
+void hola();
+
+void llamar(void (*llamado)());
 
 int main()
 {
@@ -88,20 +102,149 @@ int main()
     printf("\n");
 
     //Array
-    short int _arrayNums[] = {10,20,30,40};
+    short int _arrayNums[4] = {10,20,30,40};
+    //opcional: [4]: limite de 4
     _arrayNums[1] = 55;
 
-    printf("%i",_arrayNums[1]);
+    printf("Array: %i",_arrayNums[3]);
     printf("\ntamano array (bytes): %zu",sizeof(_arrayNums));
+    printf("\ntamano (bytes): %zu",sizeof(_arrayNums[0]));
     //devuelve 16: int=4; array=4
     //devuelve 8: short int=2; array=4
 
+    short int _array2d[2][4] = { {10,20,30,40},{1,2,3,4} };
+    //[2]: Filas, [4]: Columnas = 1:{1,2,3,4},2:{1,2,3,4}
+
+    printf("\nArray 2D: %i",_array2d[0][3]);
+
+    short int _array3d[2][2][4] =
+    {
+        {
+            {1,2,3},
+            {4,5,6}
+        },
+        {
+            {7,8,9},
+            {10,11,12}
+        }
+    };
+
+    //[2]: Capas, [2]: Filas, [3]: Columnas
+    printf("\nArray 3D: %i",_array3d[0][1][2]);
+    //para crear un string se tiene que declarar una variable tipo "char" y hacerla array[]
+
+    printf("\n");
+
+    char _string[] = "Holaaaa a todossss \"holis\"";
+    //para crear un string se tiene que declarar una variable tipo "char" y hacerla array[]
+    //se usa comillas dobles
+    char _string2[] = {'H','o','l','i','\0'};
+    // '/0':indica el final del string
+
+    _string[0] = 'J';
+    //se puede modificar los caracteres, se usa comillas simples
+
+    printf("String: %s",_string);
+    printf("\nString[0]: %c",_string[0]);
+    //como es un array se puede tomar datos tambien con un index
+
+    printf("\n%s",_string2);
+
+    printf("\n%zu",strlen(_string));
+    //string.h: strlen: largo string
+
+    strcat(_string,_string2);
+    //el resultado se almacena en la primera variable
+
+    printf("\n%s",_string); 
+    
+    int _scanNum;
+    char _scanChar;
+
+    printf("\nEscribe un numero y letra: ");
+    //scanf("%i %c",&_scanNum,&_scanChar);
+    printf("Numero: %i",_scanNum);
+    printf("\nLetra: %c",_scanChar);
+
+    char _name[30];
+    //es importante declarar el tamano
+
+    printf("\n\nEscribe nombre: ");
+    fgets(_name,sizeof(_name),stdin);
+    printf("Hola %s",_name);
+
+    int _ptrNum = 5;
+    int *_ptr = &_ptrNum;
+
+    printf("\n%p %i",_ptr,*_ptr);
+
+    int _ptrArray[] = {10,20,30,40,50};
+    _ptr = _ptrArray;
+
+    *_ptr = 30;
+    printf("\n%i",_ptrArray[0]);
+    printf("\n%i %i",*_ptr,*(_ptr+1));
+
+    printf("\nFuncion suma: %i",sum(10,15));
+
+
+    void (*_funcArray[3])() = {message1,message2,message3};
+
+    for (int i = 0;i<3;i++)
+    {
+        ln();
+        _funcArray[i]();
+    }
+
+    ln();
+    llamar(hola);
 
     return 0;
 }
 
+int sum(int a,int b)
+{
+    int result = a+b;
+    return result;
+}
+
+void message1()
+{
+    printf("Mensaje 1");
+}
+
+void message2()
+{
+    printf("Mensaje 2");
+}
+
+void message3()
+{
+    printf("Mensaje 3");
+}
+
+void ln()
+{
+    printf("\n");
+}
+
+void hola()
+{
+    printf("Holas");
+}
+
+void llamar(void (*llamado)())
+{
+    ln();
+    printf("Antes Holas");
+    ln();
+    llamado();
+    ln();
+    printf("Despues Holas");
+}
+
 /*
-%d p %i: int
+%d o %i: int
 %f: float
 %.1f: float, 1 decimal
 %lf: double
@@ -115,4 +258,6 @@ int main()
 %lu: unsigfned long int
 %llu: unsigned long long int
 %Lf: long double
+
+%p: puntero
 */
